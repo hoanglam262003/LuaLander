2f477d8e924845c2c09acc746985c7d3df61e898
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-10-01 12:30:16 +0700

    Turret

---
+++ Assets/Scripts/BulletCannon.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class BulletCannon : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float lifeTime = 3f;
+
+    private Rigidbody2D rb;
+    private Collider2D bulletCollider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bulletCollider = GetComponent<Collider2D>();
+    }
+
+    private void Start()
+    {
+        rb.linearVelocity = transform.up * speed;
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<FuelPickUp>() != null || other.GetComponent<Coins>() != null)
+        {
+            Physics2D.IgnoreCollision(bulletCollider, other);
+            return;
+        }
+        Destroy(gameObject);
+    }
+}
